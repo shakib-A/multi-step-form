2bff3243ed37f89483115ded39e48d5f9e237303
unknown <avesta9312@gmail.com>
2023-12-06 17:36:27 +0330

    YUP package installed and step one form validation is complited

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Navigation from './components/Navigation'
 
 const App = () => {
   return (
-    <div className='flex flex-col justify-center items-center md:flex-row md:justify-start bg-White top-0 mt-10 md:mt-0 md:h-[650px] md:w-[830px] md:p-3 md:rounded-xl'>
+    <div className='flex flex-col justify-center items-center md:flex-row md:justify-start md:items-start bg-White top-0 mt-10 md:mt-0 md:h-[650px] md:w-[830px] md:p-3 md:pr-0 md:rounded-xl p-4 rounded-xl'>
         <Navigation />
-      <div className='md:flex md:flex-col'>
-        <StepOne />
+      <div className='md:flex md:flex-col md:h-full'>
+        {/* <StepOne /> */}
         <StepTwo />
-        <StepThree />
-        <StepFour />
+        {/* <StepThree />
+        <StepFour /> */}
       </div>
     </div>
   )
--- src/components/StepOne.jsx
+++ src/components/StepOne.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
 const StepOne = () => {
+
+  const schema = yup.object({
+    name: yup.string().required(),
+    email: yup.string().email().required(),
+    phone: yup.number().required().typeError('please enter a valid phone number')
+  }).required();
+
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  })
+
+  
+
   return (
-    <div>StepOne</div>
+    <form onSubmit={handleSubmit()} className='flex flex-col items-start md:h-full'>
+      <h1 className='text-Marineblue font-bold text-2xl'>Personal info</h1>
+      <p className='text-Coolgray'>please provide your name, email address, and phone number.</p>
+      <div className='flex justify-between items-center w-full mt-5'>
+        <label htmlFor="name" className='text-Marineblue font-bold'>name</label>
+        {errors.name && <span className='text-Strawberryred text-sm tracking-tighter'>{errors.name.message}</span>}
+      </div>
+      <input {...register('name')} type="text" id="name" className={`${errors.name ? 'focus:outline-Strawberryred border-Strawberryred' : 'border-2 outline-none'} border-2 border-black w-full p-2 rounded-md`} />
+
+      <div className='flex justify-between items-center w-full mt-5'>
+        <label htmlFor="email" className='text-Marineblue font-bold'>Email Address</label>
+        {errors.email && <span className='text-Strawberryred text-sm tracking-tighter'>{errors.email.message}</span>}
+      </div>
+      <input {...register('email')} type="text" id="email" className={`${errors.email ? 'focus:outline-Strawberryred border-Strawberryred' : 'border-2 outline-none'} border-2 border-black w-full p-2 rounded-md`} />
+
+      <div className='flex justify-between items-center w-full mt-5'>
+        <label htmlFor="phone" className='text-Marineblue font-bold'>Phone Number</label>
+        {errors.phone && <span className='text-Strawberryred text-sm tracking-tighter'>{errors.phone.message}</span>}
+      </div>
+      <input {...register('phone')} type="text" id="phone" className={`${errors.phone ? 'focus:outline-Strawberryred border-Strawberryred' : 'border-2 outline-none'} border-2 border-black w-full p-2 rounded-md`} />
+
+      <div className='absolute left-0 bottom-0 w-full flex justify-between bg-White p-5 md:relative md:mt-auto md:pr-0'>
+        <input type="button" value='go back'/>
+        <input type="submit" value='next step' className='bg-Marineblue text-White p-4 rounded-xl capitalize font-bold'/>
+      </div>
+    </form>
   )
 }
 
