5c64d48d2dcef0a975df660c296ac91626cbf938
unknown <avesta9312@gmail.com>
2024-02-22 16:51:42 +0330

    deleted unwanted files

--- src/components/steps.js
+++
@@ -1,6 +0,0 @@
-import StepOne from './StepOne'
-import StepTwo from './StepTwo'
-import StepThree from './StepThree'
-import StepFour from './StepFour'
-
-export { StepOne, StepTwo, StepThree, StepFour }
